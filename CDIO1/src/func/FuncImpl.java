157552d4ed37a22d09f0db190ef8edcfdadfae6b
Cilian <32563603+Cilian@users.noreply.github.com>
2018-02-22 14:40:55 +0100

    fix

--- CDIO1/src/func/FuncImpl.java
+++ CDIO1/src/func/FuncImpl.java
@@ -8,14 +8,13 @@ import data.IUserDAO.DALException;
 import data.UserDTO;
 
 public class FuncImpl implements IFuncImpl{
+	//For generating password
+	
 	private static SecureRandom random = new SecureRandom();
-
-	/** different dictionaries used */
 	private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
 	private static final String NUMERIC = "0123456789";
 	private static final String SPECIAL = ".-_+!?=";
-
 
 	IUserDAO d;
 	UserDTO dt;
@@ -83,30 +82,23 @@ public class FuncImpl implements IFuncImpl{
 	public void exit() {
 
 	}
-
+	/**
+	 * Method will generate random string based on the parameters
+	 * 
+	 * @param len
+	 *            the length of the random string
+	 * @param dic
+	 *            the dictionary used to generate the password
+	 * @return the random password
+	 */
 	@Override
-
 	public String createPass(int len, String dic) {
-		/**
-		 * Method will generate random string based on the parameters
-		 * 
-		 * @param len
-		 *            the length of the random string
-		 * @param dic
-		 *            the dictionary used to generate the password
-		 * @return the random password
-		 */
+
 		String result = "";
 		for (int i = 0; i < len; i++) {
 			int index = random.nextInt(dic.length());
 			result += dic.charAt(index);
 		}
-		//				int len1 = 10;
-		//				System.out.println("Alphanumeric, special, length " + len + " chars: ");
-		//				String password = createPass(len1, ALPHA_CAPS + ALPHA + SPECIAL_CHARS);
-		//				System.out.println(password);
-		//				System.out.println();	    
-
 		return result;
 	}
 }
