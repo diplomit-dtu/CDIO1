bf2e2e52ac25fd1e58dbcf8926380dc4fc51e4a0
Patrick Marcell Madsen <s175209@student.dtu.dk>
2018-02-22 13:24:53 +0100

    changed the interface for the functionlayer

--- CDIO1/src/func/IFuncImpl.java
+++ CDIO1/src/func/IFuncImpl.java
@@ -7,10 +7,10 @@ import data.IUserDAO.DALException;
 
 public interface IFuncImpl {
 	UserDTO getUser(int userId) throws DALException; //return objekt and use toString in tui
-	List<UserDTO> getUserList(); //same concept
-	void createUser(String userName, String ini, List<String> roles, String cpr); //create userobjekt in funcImpl and ask for neccesary data
+	List<UserDTO> getUserList() throws DALException; //same concept
+	void createUser(int userId ,String userName, String ini, List<String> roles, String cpr) throws DALException; //create userobjekt in funcImpl and ask for neccesary data
 	void updateUser(int userId, String userName, String ini, String roles, String cpr, String password) throws DALException; // same concept
-	void deleteUser(); //same concept
+	void deleteUser(int userId) throws DALException; //same concept
 	void exit(); //Shuts down program
 	String createPass();
 }
