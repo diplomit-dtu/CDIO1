5fd9cd7d8acf5abd6038200e526b4a73d3cf39b9
rallerG <raller799@live.dk>
2018-02-23 12:19:34 +0100

    Changed conn to Connector

--- CDIO1/src/data/UserDBDAO.java
+++ CDIO1/src/data/UserDBDAO.java
@@ -37,7 +37,7 @@ public class UserDBDAO implements IUserDAO{
 	@Override
 	public void createUser(UserDTO user) throws DALException {
 		// TODO Auto-generated method stub
-		conn.doUpdate(	//NEEDS TO BE SETUP------------------------------------------------------------------------
+		Connector.doUpdate(	//NEEDS TO BE SETUP------------------------------------------------------------------------
 		"INSERT INTO personer(userID, userName, ini, roles, cpr, passwd) VALUES " + 
 		"(" + user.getUserId() + ", '" + user.getUserName() + "', '" + user.getIni() + 
 		"', '" + user.getRoles() + "', '" + user.getCpr() + "', '" + user.getPassword() + "');"
@@ -49,7 +49,7 @@ public class UserDBDAO implements IUserDAO{
 	@Override
 	public void updateUser(UserDTO user) throws DALException {
 		// TODO Auto-generated method stub
-		conn.doUpdate(	//NEEDS TO BE SETUP------------------------------------------------------------------------
+		Connector.doUpdate(	//NEEDS TO BE SETUP------------------------------------------------------------------------
 				"UPDATE personer SET " + 
 				 "userID = '" + user.getUserId() + "', userName = '" + user.getUserName() + "', ini = '" + user.getIni() + "', " + 
 				"roles = '" + user.getRoles() + "', cpr ='" + user.getCpr() + "', passwd = '" + user.getPassword() + "'" + 
@@ -60,7 +60,7 @@ public class UserDBDAO implements IUserDAO{
 	@Override
 	public void deleteUser(int userId) throws DALException {
 		// TODO Auto-generated method stub
-		conn.doUpdate(	//NEEDS TO BE SETUP------------------------------------------------------------------------
+		Connector.doUpdate(	//NEEDS TO BE SETUP------------------------------------------------------------------------
 				"DELETE FROM personer WHERE userID = " + userId + ";"
 				);
 	}
