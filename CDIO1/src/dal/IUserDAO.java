03b7e827825da13ab8e60d70217f54ecb6da7063
Christoffer Irvall Rasmussen <christoffer@irvall.com>
2020-02-15 20:20:39 +0100

    Changed some formatting

--- CDIO1/src/dal/IUserDAO.java
+++ CDIO1/src/dal/IUserDAO.java
@@ -5,27 +5,27 @@ import dto.UserDTO;
 
 public interface IUserDAO {
 
-	UserDTO getUser(int userId) throws DALException;
-	List<UserDTO> getUserList() throws DALException;
-	void createUser(UserDTO user) throws DALException;
-	void updateUser(UserDTO user) throws DALException;
-	void deleteUser(int userId) throws DALException;
-	
-	public class DALException extends Exception {
+  UserDTO getUser(int userId) throws DALException;
+  List<UserDTO> getUserList() throws DALException;
+  void createUser(UserDTO user) throws DALException;
+  void updateUser(UserDTO user) throws DALException;
+  void deleteUser(int userId) throws DALException;
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 7355418246336739229L;
+  public class DALException extends Exception {
 
-		public DALException(String msg, Throwable e) {
-			super(msg,e);
-		}
+    /**
+     *
+     */
+    private static final long serialVersionUID = 7355418246336739229L;
 
-		public DALException(String msg) {
-			super(msg);
-		}
+    public DALException(String msg, Throwable e) {
+      super(msg,e);
+    }
 
-	}
+    public DALException(String msg) {
+      super(msg);
+    }
+
+  }
 
 }
