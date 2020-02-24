f53528b6a1a15431fb239ef7c772602b64813661
Talha <buttt42@gmail.com>
2020-02-24 10:51:06 +0100

    Text reader

---
+++ CDIO1/src/Services/TxtReader.java
@@ -0,0 +1,77 @@
+package Services;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TxtReader {
+    private String[][] lines;
+    private int N_LINES = 0;
+    private File file;
+    private BufferedReader br;
+
+    public void openFile(String path, String fileName) {
+
+        try {
+            file = new File(path + fileName + ".txt");
+            br = new BufferedReader(new FileReader(file));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readLines() {
+
+        try {
+
+            //Count number of lines
+            String currentLine = br.readLine();
+            while (currentLine != null) {
+                N_LINES++;
+                currentLine = br.readLine();
+            }
+
+            //Reset reader
+            br = new BufferedReader(new FileReader(file));
+
+            //Add lines to 2D array
+            lines = new String[N_LINES][2];
+            for (int i = 0; i < N_LINES; i++) {
+                String[] oneLine = br.readLine().split(",,");
+                for (int j = 0; j < 2; j++) {
+                    lines[i][j] = oneLine[j];
+                }
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getN_LINES() {
+        return N_LINES;
+    }
+
+    public String getLine(String key) {
+
+        int index = -1;
+
+        //Find corresponding index to key
+        for (int i = 0; i < N_LINES; i++) {
+            if (lines[i][0].equalsIgnoreCase(key)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            return "ERROR: Key didn't match any index";
+        } else {
+            return lines[index][1];
+        }
+    }
+}
