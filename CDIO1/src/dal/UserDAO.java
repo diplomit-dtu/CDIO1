4b02429a424a37a57bfdf937efd1a887a6cb72cd
Lasse <lasse@strunge.org>
2020-02-24 10:25:27 +0100

    overides

--- CDIO1/src/dal/UserDAO.java
+++ CDIO1/src/dal/UserDAO.java
@@ -5,19 +5,24 @@ import dto.UserDTO;
 import java.util.List;
 
 public class UserDAO implements IUserDAO {
-    UserDTO getUser(int userId) throws DALException{
+    @Override
+    public UserDTO getUser(int userId) throws DALException{
         return null;
     }
-    List<UserDTO> getUserList() throws DALException{
+    @Override
+    public List<UserDTO> getUserList() throws DALException{
         return null;
     }
-    void createUser(UserDTO user) throws DALException{
+    @Override
+    public void createUser(UserDTO user) throws DALException{
 
     }
-    void updateUser(UserDTO user) throws DALException{
+    @Override
+    public void updateUser(UserDTO user) throws DALException{
 
     }
-    void deleteUser(int userId) throws DALException{
+    @Override
+    public void deleteUser(int userId) throws DALException{
 
     }
 }
