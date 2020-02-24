b9ef6a0c39a469c57b90f2bc0427189d03a25dbf
Lasse <lasse@strunge.org>
2020-02-24 10:24:01 +0100

    added implement IUserDAO

--- CDIO1/src/dal/UserDAO.java
+++ CDIO1/src/dal/UserDAO.java
@@ -1,4 +1,23 @@
 package dal;
 
-public class UserDAO {
+import dto.UserDTO;
+
+import java.util.List;
+
+public class UserDAO implements IUserDAO {
+    UserDTO getUser(int userId) throws DALException{
+        return null;
+    }
+    List<UserDTO> getUserList() throws DALException{
+        return null;
+    }
+    void createUser(UserDTO user) throws DALException{
+
+    }
+    void updateUser(UserDTO user) throws DALException{
+
+    }
+    void deleteUser(int userId) throws DALException{
+
+    }
 }
