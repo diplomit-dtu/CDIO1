cd3d97eead38047c730423432ffec9a1cac18ea7
Volkan <s180103@student.dtu.dk>
2020-03-02 12:02:25 +0100

    ddatakom deletet

--- Datakom/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    
-    public static void main(String[] args) {
-        TUI tui = new TUI();
-        Controller con = new Controller(tui);
-        con.start();
-    }
-    
-}
