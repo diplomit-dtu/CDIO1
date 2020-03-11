96b81f562fa582010fec3813b5df2880b6116216
Alexander <alexanderlambrecht30@gmail.com>
2020-03-11 11:08:40 +0100

    func er fyldt ud

--- CDIO1/cdioD1/src/main/java/func/Func.java
+++ CDIO1/cdioD1/src/main/java/func/Func.java
@@ -1,10 +1,15 @@
 package func;
 
+import dal.IUserDAO;
 import dto.UserDTO;
 
 import java.util.List;
 
 public class Func implements IFunc {
+    IUserDAO dao;
+
+    public Func(IUserDAO dao){this.dao = dao;}
+
     @Override
     public UserDTO createUser(int userID, String userName, String cpr, String password, List<String> roles) {
         UserDTO user = new UserDTO();
@@ -13,21 +18,30 @@ public class Func implements IFunc {
         user.setUserCpr(cpr);
         user.setPassword(password);
         user.setRoles(roles);
+
+        try {
+            dao.createUser(user);
+        } catch (IUserDAO.DALException e) {
+            e.printStackTrace();
+        }
+
         return user;
     }
 
     @Override
-    public List<UserDTO> getUserList() {
-        return null;
+    public List<UserDTO> getUserList() throws IUserDAO.DALException {
+        return dao.getUserList();
     }
 
     @Override
-    public UserDTO getUser(int userID) {
-        return null;
+    public UserDTO getUser(int userID) throws IUserDAO.DALException {
+        return dao.getUser(userID);
     }
 
     @Override
-    public UserDTO deleteUser(int userID) {
-        return null;
+    public UserDTO deleteUser(int userID) throws IUserDAO.DALException {
+        UserDTO user = getUser(userID);
+        dao.deleteUser(userID);
+        return user;
     }
 }
