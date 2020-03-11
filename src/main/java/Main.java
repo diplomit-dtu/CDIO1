51b3e5549cf6edb4e43ffdbf98ae65a41264494d
Silas Rindorf <s195474@student.dtu.dk>
2020-03-11 16:29:27 +0100

    set main back to original

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,14 +10,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IUserDAO.DALException {
         Scanner in = new Scanner(System.in);
-        IUserDAO dao = new UserDAOSQL("localhost","3306","user_database");
-        for (UserDTO user :
-                dao.getUserList()) {
-            System.out.println(user);
-        }
-        System.out.println(dao.getUser(2));
-        //IFunc func = new Func(dao);
-        //CLI cli = new CLI(func, in);
-        //cli.run();
+        IUserDAO dao = new UserDAONonPersistent();
+        IFunc func = new Func(dao);
+        CLI cli = new CLI(func, in);
+        cli.run();
     }
 }
