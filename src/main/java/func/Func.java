79dc31296e03256029a51f12fcaafa0750c88495
Christoffer Irvall Rasmussen <christoffer@irvall.com>
2020-03-11 20:46:45 +0100

    Added more robustness, began on table

--- src/main/java/func/Func.java
+++ src/main/java/func/Func.java
@@ -12,7 +12,7 @@ public class Func implements IFunc {
     public Func(IUserDAO dao){this.dao = dao;}
 
     @Override
-    public UserDTO createUser(int userID, String userName, String cpr, List<String> roles) throws UserFormatException {
+    public UserDTO createUser(int userID, String userName, String cpr, List<String> roles) throws UserFormatException, DatabaseException{
         List<UserFormatException.errortypes> errorlist = new ArrayList<>();
         UserDTO user = new UserDTO();
         // Check ID
@@ -21,7 +21,7 @@ public class Func implements IFunc {
         }
 
         // Check username
-        if(2<=userName.length() && userName.length()<=20){
+        if(!(2<=userName.length() && userName.length()<=20)){
             errorlist.add(UserFormatException.errortypes.username);
         }
 
@@ -30,13 +30,16 @@ public class Func implements IFunc {
         String hyphen = "";
         try{
             int test = Integer.parseInt(cpr.substring(0,6));
-            test = Integer.parseInt(cpr.substring(8,12));
+            System.out.println(test);
+            test = Integer.parseInt(cpr.substring(7,11));
+            System.out.println(test);
             hyphen = cpr.substring(6,7);
+            System.out.println(hyphen);
         }catch(NumberFormatException | StringIndexOutOfBoundsException e){
             isInteger=false;
             hyphen = "";
         }
-        if(!(isInteger && hyphen == "-")){
+        if(!isInteger || !hyphen.equals("-")){
             errorlist.add(UserFormatException.errortypes.CPR);
         }
         // Check roles
@@ -51,25 +54,25 @@ public class Func implements IFunc {
         //TODO: Check password
 
         if(errorlist.size() > 0) {
-            throw new UserFormatException("One or more user paramaters are not correctly formatted",errorlist);
+            throw new UserFormatException("One or more user parameters are not correctly formatted",errorlist);
         }
+
         user.setUserId(userID);
         user.setUserName(userName);
         user.setUserCpr(cpr);
         user.setRoles(roles);
-
-        try {
+        try{
             dao.createUser(user);
         }catch(IUserDAO.DALException e){
-            //TODO: make better
-            System.out.println("WRONGI!!!");
+            //TODO: make better, furthermore, is the message correct Silas? Sincerely christoffer
+            throw new DatabaseException("Either there is no database, or the user you are trying to create already exists");
         }
         return user;
     }
 
     @Override
     public List<UserDTO> getUserList() throws IUserDAO.DALException {
-        return dao.getUserList();
+        return new ArrayList<>(dao.getUserList());
     }
 
     @Override
