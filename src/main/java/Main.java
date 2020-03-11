42974a9fe3fda5280a9645d6c9da9a410ea79575
Alexander <alexanderlambrecht30@gmail.com>
2020-03-11 17:00:45 +0100

    commit for pull

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import dal.IUserDAO;
 import dal.UserDAONonPersistent;
 
+import dto.UserDTO;
 import func.Func;
 import func.IFunc;
 
@@ -10,6 +11,10 @@ public class Main {
     public static void main(String[] args) throws IUserDAO.DALException {
         Scanner in = new Scanner(System.in);
         IUserDAO dao = new UserDAONonPersistent();
+        for (UserDTO user :
+                dao.getUserList()) {
+            System.out.println(user);
+        }
         IFunc func = new Func(dao);
         CLI cli = new CLI(func, in);
         cli.run();
