576fe7a690be27fefcfac9e0520a02ca6c289295
Andreas <Gerneeer@gmail.com>
2020-03-13 12:57:14 +0100

    Tests started

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,77 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.dal.UserDAONonPersistent;
+
 public class test {
+
+    NonPersistentTest nonPersistentTest = new NonPersistentTest();
+    SQLTest sqlTest = new SQLTest();
+
+}
+
+public class NonPersistentTest {
+
+    public NonPersistentTest(){
+
+    }
+
+    @Test
+    public void testCreateUser(){
+        // When creating a user with an invalid ID, i get an exception.
+        UserDAONonPersistent NonPersistent = new UserDAONonPersistent();
+        String message = "";
+        try{
+            UserDTO user1 = new UserDTO(int -1, String "Name", String "Na", String "123456-1234", String "password", String "Admin");
+            NonPersistent.createUser(user1);
+        } catch (DALException E) {
+            message = E.getMessage();
+        }
+        assertTrue(message.equals("BrugerID skal være mellem 11 og 99 (inklusivt)"))
+
+
+    }
+
+    public void testGetUser(){
+
+    }
+
+    public void testGetUserList(){
+
+    }
+
+    public void testUpdateUser(){
+
+    }
+
+    public void testDeleteUser(){
+
+    }
+}
+
+public class SQLTest{
+
+    public SQLTest(){
+
+    }
+
+
+    public void testCreateUser(){
+
+    }
+
+    public void testGetUser(){
+
+    }
+
+    public void testGetUserList(){
+
+    }
+
+    public void testUpdateUser(){
+
+    }
+
+    public void testDeleteUser(){
+
+    }
 }
