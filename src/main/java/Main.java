492a89ad2972717d6130f36d9f8501ff2b9466ed
Silas Rindorf <s195474@student.dtu.dk>
2020-03-13 12:54:46 +0100

    Fixed bugs +1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import dal.UserDAONonPersistent;
 
 //import dal.UserDAOSQL;
 import dal.UserDAOSQL;
-import dto.UserDTO;
 import func.Func;
 import func.IFunc;
 
@@ -12,7 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception{
         Scanner in = new Scanner(System.in);
-        //LOG IN FOR LOCALHOST
+        //LOG IN FOR SQL LOCALHOST
         System.out.println("Type in your SQL username and password on a separate line");
         IUserDAO dao = new UserDAOSQL("localhost","3306","user_database2",in.nextLine(),in.nextLine());
         //IUserDAO dao = new UserDAONonPersistent();
